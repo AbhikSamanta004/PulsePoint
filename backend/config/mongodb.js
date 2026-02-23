2f481d730c76df11a145a8898446304e3936f2cf
AbhikSamanta004 <abhiksamanta20@gmail.com>
2026-02-24 04:21:29 +0530

    mongourl update

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,8 +1,14 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  mongoose.connection.on("connected", () => console.log("Databse Connected"));
-  await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`);
+  mongoose.connection.on("connected", () => console.log("Database Connected"));
+
+  // Only append /prescripto if it's not already in the URI
+  const uri = process.env.MONGODB_URI.includes('?')
+    ? process.env.MONGODB_URI.split('?')[0].endsWith('/prescripto') ? process.env.MONGODB_URI : process.env.MONGODB_URI.replace(/\/?(\?.*)?$/, '/prescripto$1')
+    : process.env.MONGODB_URI.endsWith('/prescripto') ? process.env.MONGODB_URI : `${process.env.MONGODB_URI}/prescripto`;
+
+  await mongoose.connect(uri);
 };
 
 export default connectDB;
